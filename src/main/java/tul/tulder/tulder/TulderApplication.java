f1d86020a47a23d3189f894cf7949b9a3e04f010
Matěj Jiránek <145572111+matejak47@users.noreply.github.com>
2025-06-14 11:31:18 +0200

    Init comit

---
+++ src/main/java/tul/tulder/tulder/TulderApplication.java
@@ -0,0 +1,13 @@
+package tul.tulder.tulder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TulderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tul/tulder/tulder/TulderApplicationTests.java
@@ -0,0 +1,13 @@
+package tul.tulder.tulder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
